7de4f84b91376a64a2a2502a7322748e476be0c5
PabloNaranjo78 <67924944+PabloNaranjo78@users.noreply.github.com>
2021-03-03 18:33:01 -0600

    Primer commit

---
+++ cmake-build-debug/CMakeFiles/TareaExtraclase1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TareaExtraclase1.dir/main.cpp.o"
+  "TareaExtraclase1"
+  "TareaExtraclase1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TareaExtraclase1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
